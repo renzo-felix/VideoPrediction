239895c50afd8ffc12f93e6242d34b87c778777f
ArianaVillegas <arianavssz25@gmail.com>
2026-04-21 09:19:19 -0500

    Add steering methods, baselines, and model loaders with VideoMAE and V-JEPA support

---
+++ experiments/src/models/base.py
@@ -0,0 +1,32 @@
+import torch
+
+class ActivationExtractor:
+    def __init__(self, model, layer_idx, model_name='vjepa'):
+        self.model = model
+        self.layer_idx = layer_idx
+        self.model_name = model_name
+        self.activations = {}
+        self.hooks = []
+
+        block = self.model.blocks[layer_idx]
+        hook = block.register_forward_hook(self._hook_fn)
+        self.hooks.append(hook)
+
+    def _hook_fn(self, module, input, output):
+        self.activations[self.layer_idx] = output.detach().cpu()
+
+    def __call__(self, x):
+        self.activations = {}
+        with torch.no_grad():
+            if self.model_name == 'videomae':
+                B = x.shape[0]
+                num_patches = self.model.patch_embed.num_patches
+                mask = torch.zeros(B, num_patches, dtype=torch.bool, device=x.device)
+                _ = self.model(x, mask=mask)
+            else:
+                _ = self.model(x)
+        return self.activations
+
+    def remove_hooks(self):
+        for hook in self.hooks:
+            hook.remove()
